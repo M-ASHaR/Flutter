4565256c1028ef1dd55eed90e8c9adbc7c20e976
M-ASHaR <itisashar3007@gmail.com>
2023-02-19 02:34:09 +0500

    starting journey with Flutter app development

---
+++ lib/practice.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  print('Welcome to dart');
+
+  stdout.write('Enter you name: ');
+  var name = stdin.readLineSync();
+
+  print('Welcome, $name');
+  var Ashar = new Human();
+
+  // Declaration of variables
+  int a;
+}
+
+class Human {
+  Human();
+}
